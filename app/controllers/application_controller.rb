63a3e08b8839b64664b1017c9ecc3d6290a34f71
LongMan <alx.gsv@gmail.com>
2010-11-07 01:53:53 +0300

    spelling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   def convert
     render :text => "Nothing to convert", :status => 404 and return if !params[:uri]
     @ebook = Ebook.find_or_create(params[:uri])
-    send_data File.open(@ebook.filename(params[:format])).read, :filename => File.basename(@ebook.name)
+    send_data File.open(@ebook.filename(params[:format])).read, :filename => File.basename(@ebook.filename)
   end
 end
