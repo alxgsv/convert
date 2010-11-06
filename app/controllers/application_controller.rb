072db418ec95321ed32caf198c5c3b1068fa2008
LongMan <alx.gsv@gmail.com>
2010-11-07 01:54:55 +0300

    format customization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   def convert
     render :text => "Nothing to convert", :status => 404 and return if !params[:uri]
     @ebook = Ebook.find_or_create(params[:uri])
-    send_data File.open(@ebook.filename(params[:format])).read, :filename => File.basename(@ebook.filename)
+    send_data File.open(@ebook.filename(params[:format])).read, :filename => File.basename(@ebook.filename(params[:format]))
   end
 end
