9c46d4baa76854a0af38f18f8d85c470c2313922
convert <convert@longman-server.(none)>
2013-04-02 10:45:46 +0200

    default format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,8 @@ class ApplicationController < ActionController::Base
 
   def convert
     render :text => "Nothing to convert", :status => 404 and return if !params[:uri]
+    chosen_format = params[:format] || "fb2"
     @ebook = Ebook.find_or_create(params[:uri])
-    send_data File.open(@ebook.filename(params[:format])).read, :filename => File.basename(@ebook.filename(params[:format]))
+    send_data File.open(@ebook.filename(chosen_format)).read, :filename => File.basename(@ebook.filename(chosen_format))
   end
 end
